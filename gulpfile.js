0f4b64e04fdcf0c9b163aa9ef36889d0f327cb04
seunggyu <seunggyu0128@gmail.com>
2018-01-14 09:30:56 +1300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  })
+
+  watch('./app/app.js', function() {
+    browserSync.reload();
+  })
+});
